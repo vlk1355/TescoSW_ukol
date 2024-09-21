6d20ba0854dde76fc21238956ce78b352924b192
vlk135 <120504588+vlk1355@users.noreply.github.com>
2024-09-22 00:46:12 +0200

    Vytvoreni trid
    Vytvoreni tridy pro tvorbu zaznamu

---
+++ TescoSw_ProgramatorBE/Zaznamy/Zaznam.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TescoSw_ProgramatorBE.Zaznamy
+{
+    internal class Zaznam
+    {
+        public string Model { get; private set; }
+        public DateTime DatumProdeje { get; private set; }
+        public double cena { get; private set; }
+        public double Dph { get; private set; }
+
+        public Zaznam(string model, DateTime datumProdeje, double cena, double dph)
+        {
+            Model = model;
+            DatumProdeje = datumProdeje;
+            this.cena = cena;
+            Dph = dph;
+        }
+    }
+}
